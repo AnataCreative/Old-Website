4275764aaa33f3d96a5c9cfe97d6cf6949be761c
Ibe Vanmeenen <ibe@gents.be>
2015-07-24 21:21:50 +0200

    update

--- app/js/app.js
+++ app/js/app.js
@@ -22,19 +22,4 @@ anatacreative.app = function(undefined) {
     }();
 };
 
-var ready = function(fn) {
-    // Sanity check
-    if (typeof(fn) !== 'function') return;
-
-    // If document is already loaded, run method
-    if (document.readyState === 'complete') {
-        return fn();
-    }
-
-    // Otherwise, wait until document is loaded
-    document.addEventListener('DOMContentLoaded', fn, false);
-};
-
-ready(function() {
-    anatacreative.app();
-});
+anatacreative.app();
