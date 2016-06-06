c73fbb066f3e3cf1fe409efe428a77d89180e4ed
Ibe Vanmeenen <ibe@gents.be>
2016-06-06 21:35:26 +0200

    start redesign

--- app/js/app.js
+++ app/js/app.js
@@ -8,44 +8,39 @@ anatacreative.app = function(undefined) {
 
 
     // Scroll
-    var appScroll = function() {
-        // Put functions in the '_update' function :-)
-        var _onScroll, _requestTick, _update,
-            latestKnownScrollY = 0,
-            ticking = false;
-
-        _onScroll = function() {
-            latestKnownScrollY = window.pageYOffset;
-            _requestTick();
-        };
-
-        _requestTick = function() {
-            if (!ticking) {
-                window.requestAnimationFrame(_update);
-            }
-            ticking = true;
-        };
-
-        _update = function() {
-            ticking = false;
-            var currentScrollY = latestKnownScrollY;
-
-            anatacreative.headerScroll.updateScroll(currentScrollY);
-        };
-
-        // scroll mousewheel wheel
-        window.onscroll = function(e) {
-            _onScroll();
-        };
-    };
+    // var appScroll = function() {
+    //     // Put functions in the '_update' function :-)
+    //     var _onScroll, _requestTick, _update,
+    //         latestKnownScrollY = 0,
+    //         ticking = false;
+    //
+    //     _onScroll = function() {
+    //         latestKnownScrollY = window.pageYOffset;
+    //         _requestTick();
+    //     };
+    //
+    //     _requestTick = function() {
+    //         if (!ticking) {
+    //             window.requestAnimationFrame(_update);
+    //         }
+    //         ticking = true;
+    //     };
+    //
+    //     _update = function() {
+    //         ticking = false;
+    //         var currentScrollY = latestKnownScrollY;
+    //     };
+    //
+    //     // scroll mousewheel wheel
+    //     window.onscroll = function(e) {
+    //         _onScroll();
+    //     };
+    // };
 
 
     // Init
     var init = function() {
-        anatacreative.headerScroll();
         anatacreative.forms();
-
-        appScroll();
     }();
 };
 
